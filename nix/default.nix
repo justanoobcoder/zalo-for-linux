6c2df3cc9770d50367c4833c1e66683c5cfe4c76
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-01-20 09:31:27 +0700

    bump to 25.12.11

--- nix/default.nix
+++ nix/default.nix
@@ -1,43 +1,41 @@
-{ lib
-, appimageTools
-, fetchurl
-, nixpkgs
-}:
-
-let
-  version = "25.11.20";
+{
+  lib,
+  nixpkgs,
+}: let
+  version = "25.12.11";
+  zadark_version = "26.1-b38f07c";
   pname = "zalo";
 
   src = nixpkgs.fetchurl {
-    url = "https://github.com/doandat943/zalo-for-linux/releases/download/${version}/Zalo-${version}+ZaDark-25.10-b38f07c.AppImage";
-    sha256 = "sha256-fNq0IiP05Fch1HXJzxk+GvOl6+s8OhDA89wi5CcRc28=";
+    url = "https://github.com/doandat943/zalo-for-linux/releases/download/${version}/Zalo-${version}+ZaDark-${zadark_version}.AppImage";
+    sha256 = "sha256-8Xfz9XXpUKn+Kf3xJbUjNmSgOC0aJYFc1SUPCF8W5gI=";
   };
 
-  appimageContents = nixpkgs.appimageTools.extractType1 { inherit pname version src; };
+  appimageContents = nixpkgs.appimageTools.extractType1 {inherit pname version src;};
 in
-nixpkgs.appimageTools.wrapType2 rec {
-  inherit pname version src;
+  nixpkgs.appimageTools.wrapType2 rec {
+    inherit pname version src;
 
-  extraInstallCommands = ''
-    mkdir -p $out/share/applications
+    extraInstallCommands = ''
+      mkdir -p $out/share/applications
 
-    cp ${appimageContents}/zalo-for-linux.desktop $out/share/applications/${pname}.desktop
+      cp ${appimageContents}/zalo-for-linux.desktop $out/share/applications/${pname}.desktop
 
-    substituteInPlace $out/share/applications/${pname}.desktop \
-      --replace 'Exec=AppRun' 'Exec=${meta.mainProgram}'
+      substituteInPlace $out/share/applications/${pname}.desktop \
+        --replace 'Exec=AppRun' 'Exec=${meta.mainProgram}'
 
-    mkdir -p $out/share/icons/hicolor/512x512/apps
-    cp ${appimageContents}/usr/share/icons/hicolor/512x512/apps/zalo-for-linux.png $out/share/icons/hicolor/512x512/apps/${pname}.png
+      mkdir -p $out/share/icons/hicolor/512x512/apps
+      cp ${appimageContents}/usr/share/icons/hicolor/512x512/apps/zalo-for-linux.png $out/share/icons/hicolor/512x512/apps/${pname}.png
 
-    substituteInPlace $out/share/applications/${pname}.desktop \
-      --replace 'Icon=zalo-for-linux' 'Icon=${pname}'
-  '';
+      substituteInPlace $out/share/applications/${pname}.desktop \
+        --replace 'Icon=zalo-for-linux' 'Icon=${pname}'
+    '';
 
-  meta = {
-    description = "Unofficial port of Zalo messaging application for Linux systems.";
-    homepage = "https://zalo.me";
-    license = lib.licenses.mit;
-    platforms = lib.platforms.linux;
-    mainProgram = "zalo";
-  };
-}
+    meta = {
+      description = "Unofficial port of Zalo messaging application for Linux systems.";
+      homepage = "https://zalo.me";
+      license = lib.licenses.mit;
+      platforms = lib.platforms.linux;
+      mainProgram = "zalo";
+    };
+  }
