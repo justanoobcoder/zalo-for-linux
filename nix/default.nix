f14aead614eb2fe0a7b7ce534aff2cdee7d97663
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-02-05 16:33:49 +0700

    update to 26.1.20

--- nix/default.nix
+++ nix/default.nix
@@ -2,13 +2,13 @@
   lib,
   nixpkgs,
 }: let
-  version = "26.1.10";
+  version = "26.1.20";
   zadark_version = "26.1-b38f07c";
   pname = "zalo";
 
   src = nixpkgs.fetchurl {
     url = "https://github.com/doandat943/zalo-for-linux/releases/download/${version}/Zalo-${version}+ZaDark-${zadark_version}.AppImage";
-    sha256 = "sha256-5lBbPuVIo1AmJjABBQAEjaKzSqrwyKCdZlayOjv9dSk=";
+    sha256 = "sha256-HrZDnwZM7V7xxabRlu73shlVraMksYmNNlP5Mo+/MtI=";
   };
 
   appimageContents = nixpkgs.appimageTools.extractType1 {inherit pname version src;};
